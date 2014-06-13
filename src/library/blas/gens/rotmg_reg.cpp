e75d11dc840a010bea5285dc49d67ee8fabc625c
Christian Kellner <christian@kellner.me>
2014-06-13 23:27:02 +0200

    Fix c&p bug in rotmg gen introduced by addBuildOpt change

--- src/library/blas/gens/rotmg_reg.cpp
+++ src/library/blas/gens/rotmg_reg.cpp
@@ -97,7 +97,7 @@ setBuildOpts(
     const CLBlasKargs *kargs = (const CLBlasKargs *)(&step->args);
 	if ( kargs->dtype == TYPE_DOUBLE || kargs->dtype == TYPE_COMPLEX_DOUBLE)
 	{
-		addBuildOpt( buildOptStr, BUILD_OPTS_MAXLEN, "-DCOMPLEX");
+		addBuildOpt( buildOptStr, BUILD_OPTS_MAXLEN, "-DDOUBLE_PRECISION");
 	}
 
 	return;
