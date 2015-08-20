5fd479d23afc1332f4ea666bb66e495a662c47cf
David Tanner <guacamoleo@gmail.com>
2015-08-20 09:09:21 -0500

    changing user to cpp files

---
+++ src/library/blas/AutoGemm/UserGemmKernelSources/UserGemmKernelSources.cpp
@@ -0,0 +1,36 @@
+/*******************************************************************************
+ * This file is NOT auto-generated; populate it with hand-written kernels
+ ******************************************************************************/
+
+#ifndef USER_GEMM_SOURCE_INCLUDES_CPP
+#define USER_GEMM_SOURCE_INCLUDES_CPP
+
+#include "UserGemmKernelSources/sgemm_Col_NN_B0_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NN_B0_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NN_B0_MX096_NX096_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NN_B1_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NN_B1_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NN_B1_MX096_NX096_KX16_src.cpp"
+
+#include "UserGemmKernelSources/sgemm_Col_NT_B0_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NT_B0_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NT_B0_MX096_NX096_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NT_B1_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NT_B1_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_NT_B1_MX096_NX096_KX16_src.cpp"
+
+#include "UserGemmKernelSources/sgemm_Col_TN_B0_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_TN_B0_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_TN_B0_MX096_NX096_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_TN_B1_MX032_NX032_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_TN_B1_MX064_NX064_KX16_src.cpp"
+#include "UserGemmKernelSources/sgemm_Col_TN_B1_MX096_NX096_KX16_src.cpp"
+
+#include "UserGemmKernelSources/dgemm_Col_NN_B0_MX048_NX048_KX08_src.cpp"
+#include "UserGemmKernelSources/dgemm_Col_NN_B1_MX048_NX048_KX08_src.cpp"
+#include "UserGemmKernelSources/dgemm_Col_NT_B0_MX048_NX048_KX08_src.cpp"
+#include "UserGemmKernelSources/dgemm_Col_NT_B1_MX048_NX048_KX08_src.cpp"
+#include "UserGemmKernelSources/dgemm_Col_TN_B0_MX048_NX048_KX08_src.cpp"
+#include "UserGemmKernelSources/dgemm_Col_TN_B1_MX048_NX048_KX08_src.cpp"
+
+#endif
