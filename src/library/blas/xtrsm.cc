16b0f9eb2e7d156760b304f872372371a111a058
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-10-28 15:30:41 +0000

    add missing include on stdlib.h
    Gbp-Pq: Name add-missing-stdlib-include.patch

--- src/library/blas/xtrsm.cc
+++ src/library/blas/xtrsm.cc
@@ -14,7 +14,7 @@
  * limitations under the License.
  * ************************************************************************/
 
-
+#include <stdlib.h>
 #include <string.h>
 #include <clBLAS.h>
 
