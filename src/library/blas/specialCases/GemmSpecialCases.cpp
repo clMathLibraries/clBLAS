b556d736dd8258b2dde817940be0b3dcecde9708
Mikhail Ivanov <wjpmuxa@yandex.ru>
2018-10-24 19:23:06 +0300

    cleanup

--- src/library/blas/specialCases/GemmSpecialCases.cpp
+++ src/library/blas/specialCases/GemmSpecialCases.cpp
@@ -15,7 +15,6 @@
 * ************************************************************************/
 
 #include <algorithm>
-#include <iostream>
 
 #include "GemmSpecialCases.h"
 #include "UserGemmKernelSources/UserGemmKernelSourceIncludes.h"
