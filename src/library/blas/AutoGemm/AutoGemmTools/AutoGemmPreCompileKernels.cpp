9c0f5cfa450c3a43b48aae8c3cc8bd6c5c7bd21f
szykk1993 <kyu_115s@hotmail.com>
2018-11-26 14:58:53 -0500

    Fix OpenCL include in AutoGemmPrecompileKernels.cpp on MacOS.

--- src/library/blas/AutoGemm/AutoGemmTools/AutoGemmPreCompileKernels.cpp
+++ src/library/blas/AutoGemm/AutoGemmTools/AutoGemmPreCompileKernels.cpp
@@ -40,7 +40,11 @@
 #define stat _stat
 #endif
 
-#include "CL/opencl.h"
+#if defined(__APPLE__) || defined(__MACOSX)
+#include <OpenCL/opencl.h>
+#else
+#include <CL/opencl.h>
+#endif
 //#include "naive_blas.cpp"
 //using namespace NaiveBlas;
 #include "AutoGemmIncludes/AutoGemmKernelsToPreCompile.h"
